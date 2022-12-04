c9e5d7c163ef64b8e1cffc30162759d57a6a234e
JckL16 <75186269+JckL16@users.noreply.github.com>
2022-12-04 03:23:54 +0100

    Fixed ratio

--- MinHandDetect.py
+++ MinHandDetect.py
@@ -6,10 +6,12 @@ detector = Module.handDetector()
 
 cap = cv2.VideoCapture(0)
 
-used_landmarks = [0, 2, 4, 5, 8,  9, 12, 13, 16, 17, 20]
+used_landmarks = [0, 2, 4, 5, 8, 9, 12, 13, 16, 17, 20]
+
+decimal_precision = 7
 
 while True:
-    _,img=cap.read()
+    _, img=cap.read()
 
     h, w, _ = img.shape
 
@@ -18,12 +20,10 @@ while True:
 
     if len(landmarks) != 0:
         for current_hand in landmarks:
-
             if len(current_hand) == 21:
                 x_max, x_min, y_max, y_min = 0, w, 0, h
                 for landmark in current_hand:
-                    x = int(landmark[1] * w)
-                    y = int(landmark[2] * h)
+                    x, y = int(landmark[1] * w), int(landmark[2] * h)
 
                     if x > x_max:
                         x_max = x
@@ -34,29 +34,21 @@ while True:
                     if y < y_min:
                         y_min = y
 
-                width = x_max - x_min
-                height = y_max - y_min
+                width, height = x_max - x_min, y_max - y_min
 
-                '''print(x_min, x_max)
-                print(y_min, y_max)
-                print(width, height)
-                print(int(current_hand[0][1] * w) - x_min, int(current_hand[0][2]*h) - y_min)
-                print()'''
-
+                # print(round((int(current_hand[0][1] * w) - x_min) / width, decimal_precision), round((int(current_hand[0][2]*h) - y_min) / height, decimal_precision))
+                # print()
 
                 cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-
                 new_poses = []
 
-                cv2.rectangle(img, (x_min, y_min), (int(current_hand[0][1] * w), int(current_hand[0][2]*h)), (0, 255, 0), 2)
-
                 for index in used_landmarks:
+                    new_poses.append([index, round((int(current_hand[index][1] * w) - x_min) / width, decimal_precision), round((int(current_hand[index][2]*h) - y_min) / height, v)])
 
-                    #new_poses.append([index, ((int(current_hand[index][1]) * w) - x_min) / width, ((int(current_hand[index][2]) * h) - y_min) / height])
-                    # print([index, ((int(current_hand[index][1]) * w) - x_min) / width, ((int(current_hand[index][2]) * h) - y_min) / height])
-                    print((int(current_hand[index][1]) * w) - x_min, (int(current_hand[index][2]) * h) - y_min)
-
+                for row in new_poses:
+                    print(row)
+                print()
 
 
     cv2.imshow("Fuck", img)
